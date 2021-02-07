0615953a598769f3aeeb1675bf06433a31dd24bc
Eric Rabiner <ericrabiner@gmail.com>
2021-02-07 18:14:03 -0500

    add RsiParabolic strat

--- crypto.py
+++ crypto.py
@@ -4,6 +4,7 @@ class Crypto():
         self.startBalance = self.balance
         self.position = False
         self.closes = []
+        self.belowRsiMin = False
 
         self.rsiPeriod = rsiPeriod
         self.rsiMax = rsiMax
@@ -15,31 +16,13 @@ class Crypto():
     def sell(self, amount):
         self.balance += amount
 
-    def getBalance(self):
-        return self.balance
-
-    def getStartBalance(self):
-        return self.startBalance
-
-    def getPosition(self):
-        return self.position
-
     def setPosition(self, newPos):
         self.position = newPos
-
-    def getCloses(self):
-        return self.closes
 
     def addClose(self, newClose):
         if (len(self.closes) > 14):
             self.closes.pop(0)
         self.closes.append(newClose)
 
-    def getRsiPeriod(self):
-        return self.rsiPeriod
-
-    def getRsiMax(self):
-        return self.rsiMax
-
-    def getRsiMin(self):
-        return self.rsiMin
+    def setBelowRsiMin(self, newBelow):
+        self.belowRsiMin = newBelow
--- readme.md
+++ readme.md
@@ -7,10 +7,12 @@ RSI (Relative Strength Index): momentum oscillator between 0 and 100. Overbought
 ## Strategies
 
 Strategy 1:
+RsiBasic
 Buy: if RSI < 30
 Sell: if RSI > 70
 
-Strategy 2 - todo:
+Strategy 2:
+RsiParabolic
 Buy: if 1st RSI < 30 & 2nd RSI > 30 (parabola)
 Sell: if RSI > 70
 
